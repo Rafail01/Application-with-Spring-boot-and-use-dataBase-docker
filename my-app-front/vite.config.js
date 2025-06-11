c8d5c0c2a950ebe3adccdc3d9f84983b618d9948
Rafail01 <it21965@hua.gr>
2025-06-11 10:58:16 +0300

    Change localHost

--- my-app-front/vite.config.js
+++ my-app-front/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/users': {
-        target: 'http://localhost:8081',
+        target: 'http://localhost:8080',
         changeOrigin: true,
       },
     },
